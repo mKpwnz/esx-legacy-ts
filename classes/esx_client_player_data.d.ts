3993ed2fd764effdacb6e412adc6f6d5459343cb
sirjxsh <sirjxsh@gmail.com>
2023-10-29 10:32:14 +0100

    :recycle: Updated Client Player and Vehicle Props

--- classes/esx_client_player_data.d.ts
+++ classes/esx_client_player_data.d.ts
@@ -1,40 +1,70 @@
+import { Coords } from './coords'
+
 export declare interface ESXClientPlayerData {
-    identifier: string,
-    inventory: ESXClientPlayerInventoryItem[],
-    loadout: any[],
-    money: number,
-    lastPosition: {
-        x: number,
-        y: number,
-        z: number
-    },
-    job: ESXClientPlayerJob,
-    secondaryJob: ESXClientPlayerJob,
+    identifier: string
+    group: string
+    inventory: ESXClientPlayerInventoryItem[] | ESXCLientPlayerOSXItem[]
+    maxWeight: number
+    firstName: string
+    lastName: string
+    sex: string
+    dateofbirth: string
+    height: number
+    dead: boolean
+    loadout: any[]
+    money: number
+    coords: Coords
+    job: ESXClientPlayerJob
+    secondaryJob: ESXClientPlayerJob
     accounts: ESXClientPlayerAccount[]
+    metadata: ESXClientPlayerMetaData
 }
 
 export declare interface ESXClientPlayerInventoryItem {
-    count: number,
-    rare: boolean,
-    limit: number,
-    label: string,
-    name: string,
-    usable: boolean,
+    count: number
+    rare: boolean
+    limit: number
+    label: string
+    name: string
+    usable: boolean
     canRemove: boolean
 }
 
 export declare interface ESXClientPlayerJob {
-    name: string,
-    grade: number,
-    grade_name: string,
-    grade_salary: number,
-    label: string,
-    skin_female: any[],
+    name: string
+    grade: number
+    grade_name: string
+    grade_salary: number
+    label: string
+    skin_female: any[]
     skin_male: any[]
 }
 
 export declare interface ESXClientPlayerAccount {
-    money: number,
-    name: string,
+    money: number
+    name: string
     label: string
+    round: boolean // This is not in the documentation, but it is in the code
+    index: number // This is not in the documentation, but it is in the code
+}
+
+export declare interface ESXCLientPlayerOSXItem {
+    label: string
+    weight: number
+    info: any[]
+    name: string
+    can_remove: boolean
+    x: number
+    y: number
+    description: string
+    amount: number
+    category: string
+    id: string
+    metadata: any[]
+    rare: boolean
+}
+
+export declare interface ESXClientPlayerMetaData {
+    armor: number
+    health: number
 }
--- classes/esx_client_vehicle_props.d.ts
+++ classes/esx_client_vehicle_props.d.ts
@@ -1,64 +1,90 @@
 export declare interface ESXClientVehicleProps {
-    model?: number,
-    plate?: string,
-    plateIndex?: number,
-    bodyHealth?: number,
-    engineHealth?: number,
-    fuelLevel?: number,
-    dirtLevel?: number,
-    color1?: number,
-    color2?: number,
-    pearlescentColor?: number,
-    wheelColor?: number,
-    wheels?: number,
-    windowTint?: number,
+    model?: number
+    doorsBroken?: boolean[]
+    windowsBroken?: boolean[]
+    tyreBurst?: boolean[]
+    tyresCanBurst?: boolean
+    plate?: string
+    plateIndex?: number
+
+    bodyHealth?: number
+    engineHealth?: number
+    tankHealth?: number
+
+    fuelLevel?: number
+    dirtLevel?: number
+    color1?: number
+    color2?: number
+    customPrimaryColor?: [number, number, number]
+    customSecondaryColor?: [number, number, number]
+
+    pearlescentColor?: number
+    wheelColor?: number
+
+    dashboardColor?: number
+    interiorColor?: number
+
+    wheels?: number
+    windowTint?: number
+    xenonColor?: number
+    customXenonColor?: [number, number, number]
+
     neonEnabled?: [number, number, number, number]
-    extras?: any,
-    neonColor?: [number, number, number],
-    tyreSmokeColor?: [number, number, number],
-    modSpoilers?: number,
-    modFrontBumper?: number,
-    modRearBumper?: number,
-    modSideSkirt?: number,
-    modExhaust?: number,
-    modFrame?: number,
-    modGrille?: number,
-    modHood?: number,
-    modFender?: number,
-    modRightFender?: number,
-    modRoof?: number,
-    modEngine?: number,
-    modBrakes?: number,
-    modTransmission?: number,
-    modHorns?: number,
-    modSuspension?: number,
-    modArmor?: number,
-    modTurbo?: boolean,
-    modSmokeEnabled?: boolean,
-    modXenon?: number,
-    modFrontWheels?: number,
-    modBackWheels?: number,
-    modPlateHolder?: number,
-    modVanityPlate?: number,
-    modTrimA?: number,
-    modOrnaments?: number,
-    modDashboard?: number,
-    modDial?: number,
-    modDoorSpeaker?: number,
-    modSeats?: number,
-    modSteeringWheel?: number,
-    modShifterLeavers?: number,
-    modAPlate?: number,
-    modSpeakers?: number,
-    modTrunk?: number,
-    modHydrolic?: number,
-    modEngineBlock?: number,
-    modAirFilter?: number,
-    modStruts?: number,
-    modArchCover?: number,
-    modAerials?: number,
-    modTrimB?: number,
-    modTank?: number,
-    modWindows?: number,
+    neonColor?: [number, number, number]
+    extras?: any // Extras
+    tyreSmokeColor?: [number, number, number]
+
+    modSpoilers?: number
+    modFrontBumper?: number
+    modRearBumper?: number
+    modSideSkirt?: number
+    modExhaust?: number
+    modFrame?: number
+    modGrille?: number
+    modHood?: number
+    modFender?: number
+    modRightFender?: number
+    modRoof?: number
+    modRoofLivery?: number
+
+    modEngine?: number
+    modBrakes?: number
+    modTransmission?: number
+    modHorns?: number
+    modSuspension?: number
+    modArmor?: number
+
+    modTurbo?: boolean
+    modSmokeEnabled?: boolean
+    modXenon?: number
+
+    modFrontWheels?: number
+    modCustomFrontWheels?: boolean
+    modBackWheels?: number
+    modCustomBackWheels?: boolean
+
+    modPlateHolder?: number
+    modVanityPlate?: number
+    modTrimA?: number
+    modOrnaments?: number
+    modDashboard?: number
+    modDial?: number
+    modDoorSpeaker?: number
+    modSeats?: number
+    modSteeringWheel?: number
+    modShifterLeavers?: number
+    modAPlate?: number
+    modSpeakers?: number
+    modTrunk?: number
+    modHydrolic?: number
+    modEngineBlock?: number
+    modAirFilter?: number
+    modStruts?: number
+    modArchCover?: number
+    modAerials?: number
+    modTrimB?: number
+    modTank?: number
+    modWindows?: number
     modLivery?: number
+    modLightbar?: number
 }
